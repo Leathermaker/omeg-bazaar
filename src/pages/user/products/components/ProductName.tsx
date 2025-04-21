0dc0ed0aeb69043aac22930c23975edd7a1bd24e
Leathermaker <leatherwebmaker123@gmail.com>
2025-04-21 18:00:13 +0530

    new commit

--- src/pages/user/products/components/ProductName.tsx
+++ src/pages/user/products/components/ProductName.tsx
@@ -30,7 +30,7 @@ const ProductName = ({ product }: { product: Product }) => {
       <span
         className={`px-3 py-1 rounded-full text-sm font-medium ${
           product.inStock
-            ? "bg-green-100 text-green-600"
+            ? "bg-green-100 text-green-700"
             : "bg-red-100 text-red-800"
         }`}
       >
